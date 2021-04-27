c1c33bef284fb297aeab8a0a398c520172abbbe1
Rjkzavr <kolya.toropof@gmail.com>
2021-04-27 21:45:09 +0300

    Init libGDX
    create libGDX project
    create PC version game

---
+++ game/android/src/com/arkadgame/game/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.arkadgame.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.arkadgame.game.ArkadGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ArkadGame(), config);
+	}
+}
---
+++ game/desktop/src/com/arkadgame/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.arkadgame.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.arkadgame.game.ArkadGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ArkadGame(), config);
+	}
+}
