8d28659c6906e72bffc7933ca3e0188e21647ab1
Rjkzavr <kolya.toropof@gmail.com>
2021-05-13 22:56:40 +0300

    Это его первый коннект

--- game/server/index.js
+++ game/server/index.js
@@ -2,19 +2,31 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var players = [];
-player.push(Player(0, 100, 100, "idle"))
+
+class Player {
+    constructor(id, x, y, action) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.action = action;
+    }
+}
+
+players.push(new Player(0, 100, 100, "idle"))
 server.listen(8000, function(){
-	console.log("Servre is now running...");
+	console.log("Server is now running...");
 });
 
 io.on('connection', function(socket) {
 	console.log("Player Connected!");
 	socket.emit('socketID', { id: socket.id });
-	socket.emit('getPlayers', players);
-	socket.broadcast.emit('newPlayer', { id: socket.id });
 	socket.on('message', function(msg) {
-	    if (msg['event'] == 'spawn') {
-	        player.push(socket.id, msg['x'], msg['y'], msg['event'])
+	    console.log("new message: " + msg.event);
+	    if (msg.event == 'spawn') {
+	        console.log("player spawned");
+	        players.push(new Player(socket.id, msg.data.x, msg.data.y, msg.data.action));
+            socket.broadcast.emit('newPlayer', { id: socket.id });
+	        socket.emit('getPlayers', players);
 	    }
 	});
 	socket.on('disconnect', function(){
@@ -27,10 +39,3 @@ io.on('connection', function(socket) {
 		}
 	});
 });
-
-function player(id, x, y, action) {
-	this.id = id;
-	this.x = x;
-	this.y = y;
-	this.action = action;
-}
