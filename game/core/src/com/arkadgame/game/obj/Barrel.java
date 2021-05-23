c7e78db40b5db97e6b239a92fe712f8c8701a787
Niki <57631867+nikniksham@users.noreply.github.com>
2021-05-24 00:15:18 +0300

    Бочки идеальны, остальная программа тоже ничего

--- game/core/src/com/arkadgame/game/obj/Barrel.java
+++ game/core/src/com/arkadgame/game/obj/Barrel.java
@@ -24,6 +24,7 @@ public class Barrel extends CustomActor {
     private float angle = 1;
     private float speedRotation = -525f;
     private float time = 0f;
+    private float destroyTime = 0f;
     private float xSpeed = 225f;
     private float ySpeed = 0f;
     private float sx = 0f;
@@ -32,6 +33,7 @@ public class Barrel extends CustomActor {
     private float dist = 0;
     private int fr = 0;
     private int frame = 1;
+    private int destroyFrame = 0;
     private int stairsFrame = 0;
     private int sizeX = 40;
     private int sizeY = 40;
@@ -40,6 +42,7 @@ public class Barrel extends CustomActor {
     private boolean onHand = true;
     private boolean firstLand = true;
     private boolean collideStairs = false;
+    private boolean isAlive = true;
 
     public Barrel(Texture barrelTexture, Person person, float sx, float sy, Terminator terminator) {
         this.terminator = terminator;
@@ -66,6 +69,7 @@ public class Barrel extends CustomActor {
         }
     }
 
+
     public void setPinchoss(ArrayList<CustomActor> pinchoss) {
         this.pinchoss = pinchoss;
     }
@@ -73,35 +77,44 @@ public class Barrel extends CustomActor {
     public void move() {
         time = Gdx.graphics.getDeltaTime();
         if (terminator.getAgrMod()) {
-            this.remove();
-            this.type = "Remove";
+            this.isAlive = false;
         }
-        if (!onHand) {
-            if (onStairs) {
-                this.move_up(-255 * time);
-                updateTextureStairs(time * 255);
-            } else {
-                barrelRegion = new TextureRegion(barrelTexture, 32, 16, 16, 16);
-            }
+        if (!isAlive) {
             if (!this.checkCollisions() && !onStairs) {
                 this.ySpeed -= time * time * gravity / 2;
             } else {
                 this.ySpeed = 0f;
             }
             this.move_up(this.ySpeed);
-            if (!onStairs) {
-                this.move_right(xSpeed * time);
-            }
-            // this.setPosition(sx, sy); this.xSpeed = -225f; speedRotation = 525f; this.ySpeed = 0f;
-            if (this.getY() < -this.getHeight()) {
-                this.remove();
-                this.type = "Remove";
-            }
+            updateTextureDestroy(time);
         } else {
-            this.setY(this.getY() + 8);
-            barrelRegion = new TextureRegion(barrelTexture, 48, stairsFrame, 16, 16);
-            if (terminator.getFrameThrow() > 18) {
-                onHand = false;
+            if (!onHand) {
+                if (onStairs) {
+                    this.move_up(-255 * time);
+                    updateTextureStairs(time * 255);
+                } else {
+                    barrelRegion = new TextureRegion(barrelTexture, 32, 16, 16, 16);
+                }
+                if (!this.checkCollisions() && !onStairs) {
+                    this.ySpeed -= time * time * gravity / 2;
+                } else {
+                    this.ySpeed = 0f;
+                }
+                this.move_up(this.ySpeed);
+                if (!onStairs) {
+                    this.move_right(xSpeed * time);
+                }
+                // this.setPosition(sx, sy); this.xSpeed = -225f; speedRotation = 525f; this.ySpeed = 0f;
+                if (this.getY() < -this.getHeight()) {
+                    this.remove();
+                    this.type = "Remove";
+                }
+            } else {
+                this.setY(this.getY() + 8);
+                barrelRegion = new TextureRegion(barrelTexture, 48, stairsFrame, 16, 16);
+                if (terminator.getFrameThrow() > 18) {
+                    onHand = false;
+                }
             }
         }
     }
@@ -113,7 +126,7 @@ public class Barrel extends CustomActor {
                 this.setY(this.getY() - speed);
                 if (this.ySpeed < -7f) {
                     terminator.getMainGameScreen().createOffset(10, 50);
-                    if (!firstLand&&!land) {
+                    if (!firstLand && !land) {
                         land = true;
                         xSpeed *= -1;
                         speedRotation *= -1;
@@ -133,7 +146,7 @@ public class Barrel extends CustomActor {
 
 
     public void move_right(float speed) {
-        if (!onStairs) {
+        if (!onStairs && isAlive) {
             this.setX(this.getX() + speed);
             if (checkCollisions()) {
                 this.setX(this.getX() - speed);
@@ -148,12 +161,31 @@ public class Barrel extends CustomActor {
     @Override
     public void draw(Batch batch, float parentAlpha) {
         move();
-        if (onStairs || onHand) {
+        if (onStairs || onHand || !isAlive) {
             angle = 0;
         } else {
             angle += speedRotation * Gdx.graphics.getDeltaTime();
         }
         batch.draw(barrelRegion, this.getX(), this.getY(), this.getWidth() / 2f, this.getHeight() / 2f, this.getWidth(), this.getHeight(), 1, 1, angle);
+    }
+
+    public void updateTextureDestroy(float time) {
+        destroyTime += time;
+        if (destroyTime >= 0.5) {
+            destroyTime = 0;
+            if (destroyFrame < 4) {
+                destroyFrame++;
+            } else {
+                this.remove();
+                this.type = "Remove";
+            }
+            if (onStairs||onHand) {
+                this.barrelRegion = new TextureRegion(barrelTexture, 64 + 16 * destroyFrame, 16, 16, 16);
+            } else {
+                this.barrelRegion = new TextureRegion(barrelTexture, 64 + 16 * destroyFrame, 32, 16, 16);
+            }
+            setSize(sizeX, sizeY);
+        }
     }
 
     public boolean checkCollisions() {
